e1b70cad9c653dbbfcda75004d7462f881a20db5
BBI-M 1035 <bbi@BBI-M-1035.local>
2018-04-10 19:41:38 +0530

    Intents Complete

--- app/src/main/java/com/test/revise/BasicActivity.java
+++ app/src/main/java/com/test/revise/BasicActivity.java
@@ -1,52 +1,69 @@
 package com.test.revise;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import static com.test.revise.MainActivity.EXTRA_REPLY;
 
 public class BasicActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = BasicActivity.class.getSimpleName();
-    public static final String EXTRA_MESSAGE = "com.test.revise.extra.MESSAGE";
-    public static final int TEXT_REQUEST = 1;
-    private TextView showTextView;
-    private EditText sendEditText;
+    private EditText websiteText;
+    private EditText locationText;
+    private EditText shareText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        sendEditText = (EditText) findViewById(R.id.sendText);
-        showTextView = (TextView) findViewById(R.id.showText);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        Log.d(LOG_TAG,"onCreate");
+        websiteText = (EditText) findViewById(R.id.website_editText);
+        locationText = (EditText) findViewById(R.id.location_editText);
+        shareText = (EditText) findViewById(R.id.share_editText);
+
     }
 
-    public void launchMain(View view) {
+    public void openWebsite(View view) {
+        String url = websiteText.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
 
-        Log.d(LOG_TAG, "Button clicked!");
-        Intent intent = new Intent(this, MainActivity.class);
-        String message = sendEditText.getText().toString();
-        System.out.println(1 + message);
-        intent.putExtra(EXTRA_MESSAGE,message);
-        startActivityForResult(intent,TEXT_REQUEST);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(MainActivity.EXTRA_REPLY);
-                showTextView.setText(reply);
-            }
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
         }
+        else{
+            Log.d("ImplicitIntents","Can't handle this");
+        }
+    }
+
+    public void openLocation(View view) {
+        String loc = locationText.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+        else{
+            Log.d("ImplicitIntents","Can't handle this");
+        }
+    }
+
+    public void shareText(View view) {
+        String txt = shareText.getText().toString();
+        String mimeText = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeText)
+                .setText(txt)
+                .setChooserTitle("Share this text with: ")
+                .startChooser();
     }
 }
