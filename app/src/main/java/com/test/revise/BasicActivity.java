911f9fce96cb1d469fd58e4a2018e0d963e995af
BBI-M 1035 <bbi@BBI-M-1035.local>
2018-04-12 18:39:35 +0530

    Support library Commit

--- app/src/main/java/com/test/revise/BasicActivity.java
+++ app/src/main/java/com/test/revise/BasicActivity.java
@@ -4,66 +4,51 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class BasicActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = BasicActivity.class.getSimpleName();
-    private EditText websiteText;
-    private EditText locationText;
-    private EditText shareText;
+    private TextView helloTextView;
+    private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null){
+            helloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
         setContentView(R.layout.activity_basic);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
         Log.d(LOG_TAG,"onCreate");
-        websiteText = (EditText) findViewById(R.id.website_editText);
-        locationText = (EditText) findViewById(R.id.location_editText);
-        shareText = (EditText) findViewById(R.id.share_editText);
+        helloTextView = (TextView) findViewById(R.id.hello_text_view);
 
     }
 
-    public void openWebsite(View view) {
-        String url = websiteText.getText().toString();
-        Uri webpage = Uri.parse(url);
-        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-
-        if (intent.resolveActivity(getPackageManager()) != null){
-            startActivity(intent);
-        }
-        else{
-            Log.d("ImplicitIntents","Can't handle this");
-        }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("color",helloTextView.getCurrentTextColor());
     }
 
-    public void openLocation(View view) {
-        String loc = locationText.getText().toString();
-        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
-        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+    public void changeColor(View view) {
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName, "color", getApplicationContext().getPackageName());
+        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        helloTextView.setTextColor(colorRes);
 
-        if (intent.resolveActivity(getPackageManager()) != null){
-            startActivity(intent);
-        }
-        else{
-            Log.d("ImplicitIntents","Can't handle this");
-        }
-    }
 
-    public void shareText(View view) {
-        String txt = shareText.getText().toString();
-        String mimeText = "text/plain";
-        ShareCompat.IntentBuilder
-                .from(this)
-                .setType(mimeText)
-                .setText(txt)
-                .setChooserTitle("Share this text with: ")
-                .startChooser();
     }
 }
